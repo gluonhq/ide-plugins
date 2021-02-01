cbcc0bd055ca080d4bbea90ce46e2a2f985338cc
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-02-01 19:38:42 +0530

    Update license headers to 2021

--- netbeans/src/main/java/com/gluonhq/plugin/netbeans/template/iterators/GluonDesktopMultiWizardIterator.java
+++ netbeans/src/main/java/com/gluonhq/plugin/netbeans/template/iterators/GluonDesktopMultiWizardIterator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Gluon Software
+ * Copyright (c) 2018, 2021, Gluon Software
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
