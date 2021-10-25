6ed909f12670c22b09f4a8a52cc164098ec53a1c
Abhinay Agarwal <abhinay_agarwal@live.com>
2021-10-25 10:25:42 +0530

    Rename client to gluonfx

--- netbeans/src/main/java/com/gluonhq/plugin/netbeans/template/iterators/GluonBaseWizardIterator.java
+++ netbeans/src/main/java/com/gluonhq/plugin/netbeans/template/iterators/GluonBaseWizardIterator.java
@@ -116,8 +116,8 @@ public abstract class GluonBaseWizardIterator implements WizardDescriptor.Progre
         wiz.putProperty(ProjectConstants.PARAM_JAVAFX_GRADLE_PLUGIN, ProjectConstants.getJavaFXGradlePluginVersion());
         wiz.putProperty(ProjectConstants.PARAM_GLUON_GLISTEN_VERSION, ProjectConstants.getGlistenVersion());
         wiz.putProperty(ProjectConstants.PARAM_GLUON_ATTACH_VERSION, ProjectConstants.getAttachVersion());
-        wiz.putProperty(ProjectConstants.PARAM_GLUON_CLIENT_MAVEN_PLUGIN, ProjectConstants.getClientMavenPluginVersion());
-        wiz.putProperty(ProjectConstants.PARAM_GLUON_CLIENT_GRADLE_PLUGIN, ProjectConstants.getClientGradlePluginVersion());
+        wiz.putProperty(ProjectConstants.PARAM_GLUONFX_MAVEN_PLUGIN, ProjectConstants.getGluonFXMavenPluginVersion());
+        wiz.putProperty(ProjectConstants.PARAM_GLUONFX_GRADLE_PLUGIN, ProjectConstants.getGluonFXGradlePluginVersion());
         wiz.putProperty(ProjectConstants.PARAM_GLUON_GLISTEN_AFTERBURNER_VERSION, ProjectConstants.getGlistenAfterburnerVersion());
 
         handle.start();
